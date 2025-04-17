9c7887124adeed31f4fff3fe4211df3fe4190937
LevkoBe <lbeniakh@kse.org.ua>
2025-04-18 00:24:56 +0300

    feat: more variations

--- modules/utils.js
+++ modules/utils.js
@@ -10,5 +10,11 @@ export function distance(p1, p2, metric) {
   if (metric === "euclidean") return Math.sqrt(dx * dx + dy * dy);
   if (metric === "manhattan") return Math.abs(dx) + Math.abs(dy);
   if (metric === "chebyshev") return Math.max(Math.abs(dx), Math.abs(dy));
+  if (metric === "canberra")
+    return Math.abs(dx / (p2.x + 1e-8)) + Math.abs(dy / (p2.y + 1e-8));
+  if (metric === "unknown1") return Math.abs(dx) * Math.abs(dy);
+  if (metric === "unknown2") return Math.max(dx - dy, dy - dx);
+  if (metric === "unknown3") return Math.min(Math.abs(dx), Math.abs(dy));
+  if (metric === "unknown4") return Math.abs(dx) % (Math.abs(dy) + 1);
   return Infinity;
 }
